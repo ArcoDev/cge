140be59d1f21d1da086fc15ed542092aa8c55757
arcodev <chris_arco0794@hotmail.com>
2021-04-20 12:21:05 -0500

    Subiendo coontenido final de la pagina

---
+++ includes/email/email.php
@@ -0,0 +1,14 @@
+<?php
+$correoDestino = "cacosta@awsoftware.mx";
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$asunto = $_POST["asunto"];
+$mensaje = $_POST["mensaje"];
+
+$contenido = "Nombre: " .$nombre.
+"\nApellido: " .$apellido.
+"\nEmail: " .$email.
+"\nAsunto: " .$asunto.
+"\nMensaje: " .$mensaje;
+
+mail($correoDestino, "Han enviado un mensaje de contacto desde tu sitio web.", $contenido);
---
+++ js/main.js
@@ -0,0 +1,72 @@
+addEventListener("DOMContentLoaded", () => {
+    const menu = document.getElementById("menu");
+    if (menu) {
+        menu.addEventListener("click", () => {
+            const bars = document.getElementById("bars");
+            const cerrar = document.getElementById("cerrar");
+            const imgMenu = document.getElementById("img-menu");
+            const infoCorreo = document.getElementById("info-correo");
+            const logo = document.getElementById("info-logo");
+            const items = document.getElementById("items");
+            bars.classList.toggle("d-none");
+            cerrar.classList.toggle("d-none");
+            imgMenu.classList.toggle("d-none");
+            imgMenu.classList.toggle("animaImg");
+            items.classList.toggle("d-none");
+            infoCorreo.classList.toggle("d-none");
+            logo.classList.toggle("d-none");
+            items.classList.toggle("animaItems");
+        });
+    }
+});
+/* Validacion de formulario */
+function validar() {
+    var nombre, email, asunto, mensaje, expresionEmail, error, txtError, enviar;
+    nombre = document.getElementById("nombre").value;
+    email = document.getElementById("email").value;
+    asunto = document.getElementById("asunto").value;
+    mensaje = document.getElementById("mensaje").value;
+    error = document.getElementById("error");
+    exito = document.getElementById("exito");
+    txtError = document.getElementById("txtError");
+    enviar = document.getElementById("enviar");
+
+    expresionEmail = /\w+@\w+\.+[a-z]/;
+
+    if (nombre === "" || email === "" || asunto === "" || mensaje === "") {
+        error.style.opacity = "1";
+        txtError.innerHTML = "Todos los campos son obligatorios";
+        setTimeout(function() {
+            error.style.opacity = "0";
+        }, 5000);
+        return false;
+    } else if (nombre.length > 20) {
+        error.style.opacity = "1";
+        txtError.innerHTML = "Nombre demasiado largo";
+        setTimeout(function() {
+            error.style.opacity = "0";
+        }, 5000);
+        return false;
+    } else if (!expresionEmail.test(email)) {
+        error.style.opacity = "1";
+        txtError.innerHTML = "El email es invalido";
+        setTimeout(function() {
+            error.style.opacity = "0";
+        }, 5000);
+        return false;
+    } else if (asunto.length > 60) {
+        error.style.opacity = "1";
+        txtError.innerHTML = "El asunto es demasiado largo";
+        setTimeout(function() {
+            error.style.opacity = "0";
+        }, 5000);
+        return false;
+    } else if (mensaje.length > 60) {
+        error.style.opacity = "1";
+        txtError.innerHTML = "EL mensaje es demasiado largo";
+        setTimeout(function() {
+            error.style.opacity = "0";
+        }, 5000);
+        return false;
+    }
+}
